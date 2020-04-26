12c1b386e8a39e46db795717f537795656494af2
veleven <veleven@foxmail.com>
2020-04-26 20:57:30 +0800

    add some custom contents (not test)

--- termux-init.sh
+++ termux-init.sh
@@ -1,11 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# define variable
+TERMUX_HOME_CONFIG_PATH=~/.termux
+
 # change software source
 read -t 60 -n9 -p "Would you want to change the apt sources to Tsinghu Source?(y/n) " result
 if [[ $result =~ y|Y && ! `cat $PREFIX/etc/apt/sources.list` =~ tsinghua ]]; then
     sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
     sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
     sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+fi
+
+# customize key
+mkdir -p $TERMUX_HOME_CONFIG_PATH
+if [ ! -f $TERMUX_HOME_CONFIG_PATH/termux.properties ]; then
+    echo "extra-keys = [['ESC','|','/','ALT','UP','QUOTE','APOSTROPHE'], ['TAB','CTRL','~','LEFT','DOWN','RIGHT','ENTER']]" >> $TERMUX_HOME_CONFIG_PATH/termux.properties
 fi
 
 # install base softwares
@@ -21,6 +30,9 @@ sshd
 
 # open shared mobile storage
 termux-setup-storage
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 
 # Base Tutorial
 # https://www.sqlsec.com/2018/05/termux.html
